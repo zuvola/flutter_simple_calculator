fb2715b560cdac1bba7389f935eee992db049a96
zuvola <zuvola@gmail.com>
2022-03-03 09:31:40 +0900

    Supports hardware keyboard

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,5 +1,7 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_grid_button/flutter_grid_button.dart';
 import 'package:intl/intl.dart' as intl;
 
@@ -247,6 +249,19 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
 
   final List<String?> _nums = List.filled(10, '', growable: false);
   final _baseStyle = const TextStyle(fontSize: 26);
+  final _focusNode = FocusNode();
+
+  void _handleKeyEvent(int row, int col) {
+    final renderObj = context.findRenderObject();
+    if (renderObj is! RenderBox) return;
+    final cellW = renderObj.size.width / 4;
+    final cellH = renderObj.size.height / 6;
+    final pos = renderObj.localToGlobal(
+        Offset(cellW * col + cellW / 2, cellH * (row + 1) + cellH / 2));
+    GestureBinding.instance!
+      ..handlePointerEvent(PointerDownEvent(position: pos))
+      ..handlePointerEvent(PointerUpEvent(position: pos));
+  }
 
   void _initController() {
     final controller = widget.controller;
@@ -288,21 +303,167 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Expanded(
-        child: _CalcDisplay(
-          hideSurroundingBorder: widget.hideSurroundingBorder,
-          hideExpression: widget.hideExpression,
-          onTappedDisplay: widget.onTappedDisplay,
-          theme: widget.theme,
-          controller: _controller,
-        ),
+    var metaKey = false;
+    return Focus(
+      focusNode: _focusNode,
+      onKeyEvent: (node, event) {
+        if (event.logicalKey == LogicalKeyboardKey.tab) {
+          return KeyEventResult.ignored;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.shiftLeft ||
+            event.logicalKey == LogicalKeyboardKey.shiftRight) {
+          return KeyEventResult.ignored;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.metaLeft ||
+            event.logicalKey == LogicalKeyboardKey.metaRight ||
+            event.logicalKey == LogicalKeyboardKey.controlLeft ||
+            event.logicalKey == LogicalKeyboardKey.controlRight) {
+          metaKey = event is KeyDownEvent;
+          return KeyEventResult.ignored;
+        }
+        if (event is KeyUpEvent) {
+          return KeyEventResult.ignored;
+        }
+        if (metaKey) {
+          if (event.logicalKey == LogicalKeyboardKey.keyC) {
+            final data = ClipboardData(text: _controller.value.toString());
+            Clipboard.setData(data);
+            return KeyEventResult.handled;
+          }
+          if (event.logicalKey == LogicalKeyboardKey.keyV) {
+            Clipboard.getData('text/plain').then((value) {
+              final val = double.tryParse(value?.text ?? '');
+              if (val != null) {
+                _controller.setValue(val);
+              }
+            });
+            return KeyEventResult.handled;
+          }
+          return KeyEventResult.ignored;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.keyC) {
+          _handleKeyEvent(0, 0);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.delete ||
+            event.logicalKey == LogicalKeyboardKey.backspace) {
+          _handleKeyEvent(0, 1);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.percent) {
+          _handleKeyEvent(0, 2);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.slash ||
+            event.logicalKey == LogicalKeyboardKey.numpadDivide) {
+          _handleKeyEvent(0, 3);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.asterisk ||
+            event.logicalKey == LogicalKeyboardKey.numpadMultiply) {
+          _handleKeyEvent(1, 3);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.minus ||
+            event.logicalKey == LogicalKeyboardKey.numpadSubtract) {
+          _handleKeyEvent(2, 3);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.equal ||
+            event.logicalKey == LogicalKeyboardKey.enter ||
+            event.logicalKey == LogicalKeyboardKey.numpadEnter ||
+            event.logicalKey == LogicalKeyboardKey.numpadEqual) {
+          _handleKeyEvent(4, 3);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.add ||
+            event.logicalKey == LogicalKeyboardKey.numpadAdd) {
+          _handleKeyEvent(3, 3);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.period ||
+            event.logicalKey == LogicalKeyboardKey.comma ||
+            event.logicalKey == LogicalKeyboardKey.numpadDecimal) {
+          _handleKeyEvent(4, 1);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.digit0 ||
+            event.logicalKey == LogicalKeyboardKey.numpad0) {
+          _handleKeyEvent(4, 0);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.digit1 ||
+            event.logicalKey == LogicalKeyboardKey.numpad1) {
+          _handleKeyEvent(3, 0);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.digit2 ||
+            event.logicalKey == LogicalKeyboardKey.numpad2) {
+          _handleKeyEvent(3, 1);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.digit3 ||
+            event.logicalKey == LogicalKeyboardKey.numpad3) {
+          _handleKeyEvent(3, 2);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.digit4 ||
+            event.logicalKey == LogicalKeyboardKey.numpad4) {
+          _handleKeyEvent(2, 0);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.digit5 ||
+            event.logicalKey == LogicalKeyboardKey.numpad5) {
+          _handleKeyEvent(2, 1);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.digit6 ||
+            event.logicalKey == LogicalKeyboardKey.numpad6) {
+          _handleKeyEvent(2, 2);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.digit7 ||
+            event.logicalKey == LogicalKeyboardKey.numpad7) {
+          _handleKeyEvent(1, 0);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.digit8 ||
+            event.logicalKey == LogicalKeyboardKey.numpad8) {
+          _handleKeyEvent(1, 1);
+          return KeyEventResult.handled;
+        }
+        if (event.logicalKey == LogicalKeyboardKey.digit9 ||
+            event.logicalKey == LogicalKeyboardKey.numpad9) {
+          _handleKeyEvent(1, 2);
+          return KeyEventResult.handled;
+        }
+        return KeyEventResult.ignored;
+      },
+      descendantsAreFocusable: false,
+      child: GestureDetector(
+        onTap: () {
+          _focusNode.requestFocus();
+        },
+        child: Column(children: <Widget>[
+          Expanded(
+            child: _CalcDisplay(
+              hideSurroundingBorder: widget.hideSurroundingBorder,
+              hideExpression: widget.hideExpression,
+              onTappedDisplay: (a, b) {
+                _focusNode.requestFocus();
+                widget.onTappedDisplay?.call(a, b);
+              },
+              theme: widget.theme,
+              controller: _controller,
+            ),
+          ),
+          Expanded(
+            child: _getButtons(),
+            flex: 5,
+          ),
+        ]),
       ),
-      Expanded(
-        child: _getButtons(),
-        flex: 5,
-      ),
-    ]);
+    );
   }
 
   @override
@@ -327,6 +488,7 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
       hideSurroundingBorder: widget.hideSurroundingBorder,
       borderWidth: widget.theme?.borderWidth ?? 0,
       onPressed: (dynamic val) {
+        _focusNode.requestFocus();
         switch (val) {
           case '→':
             _controller.removeDigit();
