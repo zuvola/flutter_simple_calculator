3fce5b3dbe2d5b15d762d64a319b06557f90db93
Ievgenii Kovtun <ievgenii.kovtun@veeam.com>
2022-06-17 18:53:30 +0200

    Make fields calc and acLable public. Useful for redefined CalcController instances.

--- lib/src/calc_controller.dart
+++ lib/src/calc_controller.dart
@@ -5,113 +5,108 @@ import 'calculator.dart';
 
 /// Controller for calculator.
 class CalcController extends ChangeNotifier {
-  final Calculator _calc;
-  String _acLabel = 'AC';
+  final Calculator calc;
+  String acLabel = 'AC';
 
   /// Create a [CalcController] with [maximumDigits] is 10 and maximumFractionDigits of [numberFormat] is 6.
-  CalcController({maximumDigits = 10})
-      : _calc = Calculator(maximumDigits: maximumDigits);
+  CalcController({maximumDigits = 10}) : calc = Calculator(maximumDigits: maximumDigits);
 
   /// Create a [Calculator].
-  CalcController.numberFormat(intl.NumberFormat numberFormat, int maximumDigits)
-      : _calc = Calculator.numberFormat(numberFormat, maximumDigits);
+  CalcController.numberFormat(intl.NumberFormat numberFormat, int maximumDigits) : calc = Calculator.numberFormat(numberFormat, maximumDigits);
 
   /// Display string
-  String? get display => _calc.displayString;
+  String? get display => calc.displayString;
 
   /// Display value
-  double? get value => _calc.displayValue;
+  double? get value => calc.displayValue;
 
   /// Expression
-  String? get expression => _calc.expression;
-
-  /// Label for the "AC" button, "AC" or "C".
-  String get acLabel => _acLabel;
+  String? get expression => calc.expression;
 
   /// The [NumberFormat] used for display
-  intl.NumberFormat get numberFormat => _calc.numberFormat;
+  intl.NumberFormat get numberFormat => calc.numberFormat;
 
   /// Set the value.
   void setValue(double val) {
-    _calc.setValue(val);
-    _acLabel = 'C';
+    calc.setValue(val);
+    acLabel = 'C';
     notifyListeners();
   }
 
   /// Add digit to the display.
   void addDigit(int num) {
-    _calc.addDigit(num);
-    _acLabel = 'C';
+    calc.addDigit(num);
+    acLabel = 'C';
     notifyListeners();
   }
 
   /// Add a decimal point.
   void addPoint() {
-    _calc.addPoint();
-    _acLabel = 'C';
+    calc.addPoint();
+    acLabel = 'C';
     notifyListeners();
   }
 
   /// Clear all entries.
   void allClear() {
-    _calc.allClear();
+    calc.allClear();
     notifyListeners();
   }
 
   /// Clear value to zero.
   void clear() {
-    _calc.clear();
-    _acLabel = 'AC';
+    calc.clear();
+    acLabel = 'AC';
     notifyListeners();
   }
 
   /// Perform operations.
   void operate() {
-    _calc.operate();
-    _acLabel = 'AC';
+    calc.operate();
+    acLabel = 'AC';
     notifyListeners();
   }
 
   /// Remove the last digit.
   void removeDigit() {
-    _calc.removeDigit();
+    calc.removeDigit();
     notifyListeners();
   }
 
   /// Set the operation to addition.
   void setAdditionOp() {
-    _calc.setOperator('+');
+    calc.setOperator('+');
     notifyListeners();
   }
 
   /// Set the operation to subtraction.
   void setSubtractionOp() {
-    _calc.setOperator('-');
+    calc.setOperator('-');
     notifyListeners();
   }
 
   /// Set the operation to multiplication.
   void setMultiplicationOp() {
-    _calc.setOperator('×');
+    calc.setOperator('×');
     notifyListeners();
   }
 
   /// Set the operation to division.
   void setDivisionOp() {
-    _calc.setOperator('÷');
+    calc.setOperator('÷');
     notifyListeners();
   }
 
   /// Set a percent sign.
   void setPercent() {
-    _calc.setPercent();
-    _acLabel = 'C';
+    calc.setPercent();
+    acLabel = 'C';
     notifyListeners();
   }
 
   /// Toggle between a plus sign and a minus sign.
   void toggleSign() {
-    _calc.toggleSign();
+    calc.toggleSign();
     notifyListeners();
   }
 }
